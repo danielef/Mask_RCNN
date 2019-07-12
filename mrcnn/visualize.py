1ebc2755035c7406380f228437ea128af83f6e6c
Daniel Estevez <daniel.ef@gmail.com>
2019-07-12 15:01:51 -0500

    Add return fig & ax

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -164,6 +164,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    print('plt_show: {}'.format(plt_show))
+    print('auto_show: {}'.format(auto_show))
     if auto_show and plt_show:
         plt.show()
     
